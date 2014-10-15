a5a84e174339a03d589339b1ad6f7af028806d79
Rodrigo Parra <rodpar07@gmail.com>
2014-10-15 10:30:53 -0300

    Add modal navigation and change sidetag behavior

--- js/app.js
+++ js/app.js
@@ -9,19 +9,10 @@ $(document).ready(function(){
   $("#distrito").select2();
   $("#localidad").select2();
   add_filter_listeners(map);
-
-
-  $('.navbar-nav>li>a').bind('click', function (e) {
-    if($(this).attr('href') === '#section-mapa'){
-      $('#opener').show();
-    }else{
-      if ($('#slide-panel').hasClass("visible")) {
-        $('#opener').click();
-      }
-      $('#opener').hide();
-    }
-  });
+  setup_modal_navigation();
 });
+
+ 
 
 
 function draw_map () {
@@ -198,14 +189,24 @@ function draw_sidetag(map){
     var panel = $('#slide-panel');
     if (panel.hasClass("visible")) {
        panel.removeClass('visible').css({'margin-left':'-300px'});
-       $('.main-container').css({'padding-left':'0px'});
-    } else {panel.addClass('visible').css({'margin-left':'0px'});
-       $('.main-container').css({'padding-left':'300px'});
+    } else {
+      panel.addClass('visible').css({'margin-left':'0px'});
     }
     $('#opener-icon').toggleClass("glyphicon glyphicon-chevron-down");
     $('#opener-icon').toggleClass("glyphicon glyphicon-chevron-up");
     map.invalidateSize();
     return false; 
+  });
+
+  $('.navbar-nav>li>a').bind('click', function (e) {
+    if($(this).attr('href') === '#section-mapa'){
+      $('#opener').show();
+    }else{
+      if ($('#slide-panel').hasClass("visible")) {
+        $('#opener').click();
+      }
+      $('#opener').hide();
+    }
   });
 }
 
@@ -264,14 +265,60 @@ function setup_modal(){
   $('li img').on('click',function(){
     var src = $(this).attr('src');
     var img = '<img src="' + src + '" class="img-responsive"/>';
+
+    var index = $(this).parent('li').index();                   
+    var html = '';
+    html += img;                
+    html += '<div style="height:25px;clear:both;display:block;">';
+    html += '<a class="controls next" href="'+ (index+2) + '">siguiente &raquo;</a>';
+    html += '<a class="controls previous" href="' + (index) + '">&laquo; anterior</a>';
+    html += '</div>';
+
     $('#photo-modal').modal();
     $('#photo-modal').on('shown.bs.modal', function(){
-      $('#photo-modal .modal-body').html(img);
+      $('#photo-modal .modal-body').html(html);
+      //this will hide or show the right links:
+      $('a.controls').trigger('click');
     });
     $('#photo-modal').on('hidden.bs.modal', function(){
       $('#photo-modal .modal-body').html('');
     });
   });  
+}
+
+function setup_modal_navigation() {
+  $(document).on('click', 'a.controls', function(e){
+    var index = $(this).attr('href');
+    var src = $('ul.row li:nth-child('+ index +') img').attr('src');             
+    $('.modal-body img').attr('src', src);
+
+    var newPrevIndex = parseInt(index) - 1; 
+    var newNextIndex = parseInt(newPrevIndex) + 2; 
+ 
+    if($(this).hasClass('previous')){               
+        $(this).attr('href', newPrevIndex); 
+        $('a.next').attr('href', newNextIndex);
+    }else{
+        $(this).attr('href', newNextIndex); 
+        $('a.previous').attr('href', newPrevIndex);
+    }
+
+    var total = $('ul.row li').length + 1; 
+    //hide next button
+    if(total === newNextIndex){
+        $('a.next').hide();
+    }else{
+        $('a.next').show()
+    }            
+    //hide previous button
+    if(newPrevIndex === 0){
+        $('a.previous').hide();
+    }else{
+        $('a.previous').show()
+    }
+
+    return false;
+  });
 }
 
 function startLoading() {
