9a4804ada183670888f58d7322ffea8b5820b7b4
Rodrigo Parra <rodpar07@gmail.com>
2014-10-20 16:12:05 -0300

    Add real dataset

---
+++ js/viviendas.js.bkp2.js
@@ -0,0 +1,128 @@
+var viviendas = {
+    type: 'FeatureCollection',
+    features: [{
+        type: 'Feature',
+        properties: {
+            proyecto: 'Mi País, mi Casa',
+            estado: 'concluido',
+            contrato: 'RJ-45',
+            empresa: 'Constructora 1',
+            razon_social: 'Razón Social 1',
+            ruc: '2344553-1',
+            monto: 4500000,
+            codigo_licitacion: 'abc123',
+            fecha_inicio: '02/04/2013',
+            fecha_fin: '02/04/2014',
+            departamento: 'Guaira',
+            distrito: 'PASO YOBAI',
+            localidad: 'Colonia Nancen',
+            terreno: 243,
+            construido: 244,
+            cantidad_viviendas: 24
+        },
+        geometry: {
+            type: 'Point',
+            coordinates: [-56.03302392,-25.74693046]
+        }
+    },
+    {
+        type: 'Feature',
+        properties: {
+            proyecto: 'Mi País, mi Casa',
+            estado: 'concluido',
+            contrato: 'RJ-45',
+            empresa: 'Constructora 1',
+            razon_social: 'Razón Social 1',
+            ruc: '2344553-1',
+            monto: 4500000,
+            codigo_licitacion: 'abc123',
+            fecha_inicio: '02/04/2013',
+            fecha_fin: '02/04/2014',
+            departamento: 'Guaira',
+            distrito: 'Santa Catalina',
+            localidad: 'General Garay',
+            terreno: 143,
+            construido: 244,
+            cantidad_viviendas: 77
+        },
+        geometry: {
+            type: 'Point',
+            coordinates: [-56.15977723,-26.00618379]
+        }
+    },
+    {
+        type: 'Feature',
+        properties: {
+            proyecto: 'Focen',
+            estado: 'en progreso',
+            contrato: 'TRS-76',
+            empresa: 'Constructora 2',
+            razon_social: 'Razón Social 2',
+            ruc: '2344553-1',
+            monto: 765000000,
+            codigo_licitacion: 'abc123',
+            fecha_inicio: '02/04/2013',
+            fecha_fin: '',
+            departamento: 'Itapua',
+            distrito: 'Yatytay',
+            localidad: 'Barrio San Ramón',
+            terreno: 143,
+            construido: 244,
+            cantidad_viviendas: 54
+        },
+        geometry: {
+            type: 'Point',
+            coordinates: [-55.05447544,-26.67130573]
+        }
+    },
+    {
+        type: 'Feature',
+        properties: {
+            proyecto: 'Fonavis',
+            estado: 'en progreso',
+            contrato: 'ABW-44',
+            empresa: 'Constructora 3',
+            razon_social: 'Razón Social 3',
+            ruc: '2344553-1',
+            monto: 545000000,
+            codigo_licitacion: 'dsv456',
+            fecha_inicio: '02/04/2013',
+            fecha_fin: '',
+            departamento: 'Boquerón',
+            distrito: 'Mariscal Estigarribia',
+            localidad: 'Fischat San Leonardo',
+            terreno: 444,
+            construido: 221,
+            cantidad_viviendas: 45
+        },
+        geometry: {
+            type: 'Point',
+            coordinates: [-60.76300638,-23.75968437]
+        }
+    },
+    {
+        type: 'Feature',
+        properties: {
+            proyecto: 'Clase Media',
+            estado: 'concluido',
+            contrato: 'ABW-44',
+            empresa: 'Constructora 2',
+            razon_social: 'Razón Social 2',
+            ruc: '2344553-1',
+            monto: 545000000,
+            codigo_licitacion: 'dsv456',
+            fecha_inicio: '03/08/2013',
+            fecha_fin: '03/06/2014',
+            departamento: 'Guaira',
+            distrito: 'General Garay',
+            localidad: 'Virgen de Fátima',
+            terreno: 345,
+            construido: 112,
+            cantidad_viviendas: 45
+        },
+        geometry: {
+            type: 'Point',
+            coordinates: [-56.20763607,-25.95006566]
+        }
+    }]
+};
